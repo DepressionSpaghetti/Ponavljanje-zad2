28ef1364d67ac17f04181cff209bd32d0a32725e
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2023-09-11 10:39:27 +0200

    Add files via upload

---
+++ Ponavljanje-zad2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje_zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strng;
+
+            Console.WriteLine("Upisite niz znakova. ");
+            strng = Console.ReadLine();
+
+            Console.WriteLine(strng.Replace(' ', '_'));
+            Console.ReadKey();
+        }
+    }
+}
